d4eaad8469049b4f69553d0bb00fc4e0518aa479
taringamberini <taringamberini@users.noreply.github.com>
2017-08-21 23:12:58 +0200

    Fix meld configuration

--- README.md
+++ README.md
@@ -1,13 +1,22 @@
 # my-git-configuration
 My current [git](https://git-scm.com/) configuration file.
 
-## Requirement
-Git MUST be installed, even if not yet configured as I would.
 
 ## Install
+
+Git MUST be installed, even if not yet configured as I would.
+
+At command line run:
 
 ```bash
 ~$ wget --no-check-certificate https://raw.githubusercontent.com/taringamberini/my-git-configuration/master/install-gitconfig.sh
 ~$ sh install-gitconfig.sh
 ```
+
+After succesful installation remove the installation script.
+
+
+## Use
+
+I have configured git to use [meld](http://meldmerge.org/) as diff tool, therefore meld MUST be installed.
 
--- install-gitconfig.sh
+++ install-gitconfig.sh
@@ -11,8 +11,8 @@ echo "Checking for previous git configuration"
 if [ -f ~/.gitconfig ] || [ -d ~/$INSTALL_DIR ]
 then
 	echo "You already have an existing git configuration"
-	echo -n "Trying to back it up in ~/$BACKUP_DIR ... "
-    mkdir ~/$BACKUP_DIR
+	echo -n "Trying to back it up in ~/$BACKUP_DIR... "
+	mkdir ~/$BACKUP_DIR
 	mv ~/.gitconfig ~/$BACKUP_DIR
 	mv ~/$INSTALL_DIR ~/$BACKUP_DIR
 	echo "success"
@@ -20,13 +20,15 @@ fi
 
 
 echo "Installing git configuration in ~/$INSTALL_DIR"
-
-echo "Cloning my git configuration from GitHub in ~/$INSTALL_DIR"
 git clone https://github.com/taringamberini/my-git-configuration.git ~/$INSTALL_DIR
 
-echo -n "Creating symlink ... "
+echo -n "Creating symlink to git configuration... "
 ln -s ~/$INSTALL_DIR/gitconfig ~/.gitconfig
 echo "success"
 
-echo "git installation completed :-)"
+echo -n "Integrating with meld... "
+chmod 744 ~/$INSTALL_DIR/gitconfig_diff_with_meld.sh
+echo "success"
+
+echo "git configuration installed :-)"
 
